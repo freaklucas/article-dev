7e8ede7e6dd2324bfc8eb75dc05051701b0e2eaa
freaklucas <lucas.oliveira.freak@gmail.com>
2023-03-17 18:02:51 -0300

    reat:getServerSideProps p buscar diretamente no server

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,17 +1,33 @@
+import { GetServerSideProps } from "next";
 import styles from "./styles.module.css";
 import Head from "next/head";
+import { getSession } from "next-auth/react";
 
 export default function Dashboard() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>
-            Painel de tarefas
-        </title>
+        <title>Painel de tarefas</title>
       </Head>
-        <h1>
-            Página painel
-        </h1>
+      <h1>Página painel</h1>
     </div>
   );
 }
+
+export const getServerSideProps: 
+  GetServerSideProps = async ({ req }) => {
+  const session = await getSession({ req });
+
+  if (!session?.user) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
+};

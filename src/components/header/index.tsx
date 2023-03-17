50e22f55b07b744a3abe3eeb22bfa107abc04a25
freaklucas <lucas.oliveira.freak@gmail.com>
2023-03-17 14:42:50 -0300

    feat: login, logout google

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,26 +1,45 @@
-import Link from 'next/link';
-import styles from './styles.module.css';
+import Link from "next/link";
+import styles from "./styles.module.css";
+import { useSession, signIn, signOut } from "next-auth/react";
 
 export function Header() {
+  const { data: session, status } = useSession();
   return (
     <header className={styles.header}>
-        <section className={styles.content}>
-            <nav className={styles.nav}>
-                <Link href="/">
-                    <h1 className={styles.logo}>
-                        Artigos
-                        <span>+</span>
-                    </h1>
-                </Link>
-                <Link className={styles.link} 
-                    href="/dashboard">
-                    Meu painel
-                </Link>
-            </nav>
-            <button className={styles.loginButton}>
-                Acessar
-            </button>
-        </section>
+      <section className={styles.content}>
+        <nav className={styles.nav}>
+          <Link href="/">
+            <h1 className={styles.logo}>
+              Artigos
+              <span>+</span>
+            </h1>
+          </Link>
+          {
+            session?.user && (
+                <Link className={styles.link} href="/dashboard">
+                Meu painel
+              </Link>
+            )
+          }
+        </nav>
+        {status === "loading" ? (
+            <>Loading...</>
+        ) : session ? (
+          <button 
+            className={styles.loginButton} 
+            onClick={() => signOut()}
+          >
+            Olá {session?.user?.name}
+          </button>
+        ) : (
+          <button 
+            className={styles.loginButton} 
+            onClick={() => signIn("google")}
+          >
+            Acessar
+          </button>
+        )}
+      </section>
     </header>
-  )
+  );
 }

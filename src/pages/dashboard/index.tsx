a6753dcb9931f9dee58a21bd165eae9cd57dfa5c
freaklucas <lucas.oliveira.freak@gmail.com>
2023-03-19 00:25:12 -0300

    feat:criando compo do article

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,8 +1,12 @@
 import { GetServerSideProps } from "next";
 import styles from "./styles.module.css";
 import Head from "next/head";
+
 import { getSession } from "next-auth/react";
 import { TextArea } from "@/components/textarea"; 
+import { FiShare2 } from "react-icons/fi";
+import { FaTrash } from "react-icons/fa";
+
 
 export default function Dashboard() {
   return (
@@ -37,6 +41,29 @@ export default function Dashboard() {
               </button>
             </form>
           </div>
+        </section>
+        <section className={styles.articleContainer}>
+          <h1>Meus artigos</h1>
+          <article className={styles.article}>
+            <div className={styles.tagContainer}>
+              <label className={styles.tag}>PUBLICO</label>
+              <button className={styles.shareButton}>
+                <FiShare2
+                  size={22}
+                  color="#3183ff"
+                />
+              </button>
+            </div>
+            <div className={styles.articleContent}>
+              <p>Meu artigo base: foi assim...</p>
+              <button className={styles.trashButton}>
+              <FaTrash
+                  size={22}
+                  color="#ea3140"
+                />
+              </button>
+            </div>
+          </article>
         </section>
       </main>
     </div>
